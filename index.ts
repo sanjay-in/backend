30e890f6180b2362ab4a4f14f994b864872e84a1
Sanjay <127919868+sanjay-in@users.noreply.github.com>
2026-04-21 13:53:58 +0000

    update server index

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
+import userRouter from "./src/routes/user.routes";
+import { connectToDB } from './src/lib/mongo';
 
 const app = new Hono()
 
@@ -8,9 +10,14 @@ app.use("/api/*", cors({
   allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
+app.route("/api/user", userRouter);
+
 app.get('/', (c) => c.text('Hello Bun!'))
 
+  await connectToDB();
+  console.log("Database connected successfully");
+
 export default {
-  port: Bun.env.PORT ?? 4000,
+  port: Bun.env.PORT ?? 5000,
   fetch: app.fetch,
 }
