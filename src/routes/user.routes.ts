9d670c62ca5f5f0e5f4ae4b5c7fb23c5d66524d4
sanjay-in <sanjaysridharan@outlook.com>
2026-04-27 19:42:17 +0530

    add routes to update verifyEmail

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Hono } from "hono";
-import { registerUser, login, refreshToken } from "../controllers/user/user.controller"; 
+import { registerUser, login } from "../controllers/user/user.controller"; 
+import { verifyEmail } from "../lib/auth";
 
 const userRouter = new Hono();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", login);
-userRouter.post("/refresh-token", refreshToken);
+userRouter.post("/auth/hooks", verifyEmail);
 
 export default userRouter;
